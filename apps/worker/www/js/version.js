ccba28b666262feb0d9f61f4db43b515ea68968e
Vishal Vishnu Mariappan <Vishal.Mariappan@ibsplc.com>
2026-08-16 12:30:22 +0530

    ota

--- apps/worker/www/js/version.js
+++ apps/worker/www/js/version.js
@@ -1,11 +1,5 @@
-window.APP_VERSION = '1.0.0-dev';
+window.APP_VERSION = '1.0.0-20260816065751';
 window.APP_NAME = 'worker';
 window.DEFAULT_SERVER_URL = 'http://192.162.0.5:3000';
-window.getServerUrl = function() {
-    return localStorage.getItem('ota:serverUrl') || window.DEFAULT_SERVER_URL;
-};
-window.setServerUrl = function(url) {
-    if (url) localStorage.setItem('ota:serverUrl', url);
-    else localStorage.removeItem('ota:serverUrl');
-};
-
+window.getServerUrl = function() { return localStorage.getItem('ota:serverUrl') || window.DEFAULT_SERVER_URL; };
+window.setServerUrl = function(url) { if (url) localStorage.setItem('ota:serverUrl', url); else localStorage.removeItem('ota:serverUrl'); };
